6021a5dffb8b78db2b3e43fafc2e7bf5db06c46a
jarios71 <jarios@twa.es>
2022-02-12 18:28:04 +0100

    debugging

--- stage-1/test-tt-chain.js
+++ stage-1/test-tt-chain.js
@@ -3,12 +3,12 @@ const { TTBlock, TTBlockchain } = require('./tt-blockchain.js');
 const MyTTChain = new TTBlockchain();
 
 MyTTChain.add(
-  new TTBlock(Date.now().toString(), 
+  new TTBlock(Date.now().toString(),
     {
       from: 'crypto-tonterias',
       to: 'my-friend',
       amount: 100,
-      notes: 'present'
+      notes: 'A present from Jose ;)'
     }
   )
 );
